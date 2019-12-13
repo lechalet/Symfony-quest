2d810ae9e511804e36aa575530d7da55339585fc
chalet <r.bardouxx@hotmail.fr>
2019-12-13 09:28:10 +0100

    first comit

---
+++ READMElink.md
@@ -0,0 +1,2 @@
+Link for quest 12 :
+https://www.loom.com/share/3173c137ea384d64a4c81373dfa9cae6
---
+++ src/Controller/WildController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\SeasonRepository;
+use App\Entity\Episode;
+use Symfony\Component\HttpFoundation\Request;
+
+class WildController extends AbstractController
+{
+    /**
+     * @Route("/wild", name="wild_index")
+     */
+    public function index(ProgramRepository $programRepository, Request $request)
+    {
+        $programs = $programRepository->findAll();
+
+        $hyphenizedCategories = [];
+        $hyphenizedPrograms = [];
+        foreach($programs as $program) {
+            $category = $program->getCategory();
+            $hyphenizedCategories[] = strtolower(str_replace(' ', '-', $category->getName()));
+            $hyphenizedPrograms[] = strtolower(str_replace(' ', '-', $program->getTitle()));
+        }
+
+        if (!$programs) {
+          throw $this->createNotFoundException('No program found in program\'s table.');
+        }
+
+        return $this->render('wild/index.html.twig',
+          [
+              'programs' => $programs,
+              'hyphenizedCategories' => $hyphenizedCategories,
+              'hyphenizedPrograms' => $hyphenizedPrograms
+          ]
+        );
+    }
+
+    /**
+     * @Route("/wild/show/{slug<[a-z0-9-]+>}", defaults={"slug" = null}, name="wild_show")
+     */
+    public function show(?string $slug, ProgramRepository $programRepository)
+    {
+        if (!$slug) {
+            throw $this
+            ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+        
+        $showName = str_replace('-', ' ', $slug);
+        $showName = ucwords($showName);
+        $program = $programRepository->findOneBy(['title' => mb_strtolower($showName)]);
+
+        return $this->render('wild/show.html.twig', [
+            'showName' => $showName,
+            'program' => $program,
+            'hyphenizedCategory' => strtolower(str_replace(' ', '-', $program->getCategory()->getName())),
+            'hyphenizedProgram' => strtolower(str_replace(' ', '-', $program->getTitle()))
+        ]);
+    }
+
+    /**
+     * @Route("/wild/category/{categoryName<[a-z]+>}", defaults={"categoryName" = null}, name="show_category")
+     */
+    public function showByCategory(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository)
+    {
+        $category = $categoryRepository->findOneBy(['name' => ucwords(str_replace('-', ' ', $categoryName))]);
+
+        $programs = $programRepository->findBy(
+            ['category' => $category],
+            ['id' => 'DESC'],
+            3
+        );
+
+        $hyphenizedPrograms = [];
+        foreach($programs as $program) {
+            $category = $program->getCategory();
+            $hyphenizedPrograms[] = strtolower(str_replace(' ', '-', $program->getTitle()));
+        }
+
+        return $this->render('wild/category.html.twig', [
+          'programs' => $programs,
+          'categoryName' => $category->getName(),
+          'hyphenizedPrograms' => $hyphenizedPrograms
+        ]);
+    }
+
+   /**
+     * @Route("/wild/program/{programName<[a-z0-9-]+>}", defaults={"programName" = null}, name="show_program")
+     */
+    public function showByProgram(string $programName, ProgramRepository $programRepository)
+    {
+        $program = $programRepository->findOneBy(['title' => str_replace('-', ' ',$programName)]);
+
+
+        $seasons = $program->getSeasons();
+
+        return $this->render('wild/program.html.twig', [
+          'program' => $program,
+          'seasons' => $seasons,
+          'hyphenizedProgram' => strtolower(str_replace(' ', '-', $program->getTitle()))
+        ]);
+    }
+
+   /**
+     * @Route("/wild/season/{seasonId<[0-9]+>}", name="show_season")
+     */
+    public function showBySeason(int $seasonId, SeasonRepository $seasonRepository)
+    {
+        $season = $seasonRepository->findOneById($seasonId);
+
+        $program = $season->getProgram();
+        $episodes = $season->getEpisodes();
+
+        return $this->render('wild/season.html.twig', [
+          'program' => $program,
+          'season' => $season,
+          'episodes' => $episodes,
+          'hyphenizedProgram' => strtolower(str_replace(' ', '-', $program->getTitle()))
+        ]);
+    }
+
+   /**
+     * @Route("/wild/episode/{id}", name="show_episode")
+     */
+    public function showByEpisode(Episode $episode)
+    {
+        $season = $episode->getSeason();
+        $program = $season->getProgram();
+        $hyphenizedProgramTitle = strtolower(str_replace(' ', '-', $program->getTitle()));
+
+        return $this->render('wild/episode.html.twig', [
+            'episode' => $episode,
+            'season' => $season,
+            'program' => $program,
+            'hyphenizedProgramTitle' => $hyphenizedProgramTitle
+        ]);
+    }
+}
